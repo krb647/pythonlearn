d87d7803230ac868ee3f28bd20ee3244200a86d9
Karthick Raja B <120722003+krb647@users.noreply.github.com>
2023-01-17 21:24:05 +0530

    Create heads or tails.py

---
+++ heads or tails.py
@@ -0,0 +1,9 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+#Write the rest of your code below this line 👇
+random_number=random.randint(0,1)
+if random_number==0:
+    print("Tails")
+elif random_number==1:
+    print("Heads")
