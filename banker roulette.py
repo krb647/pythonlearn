8f77ca53c86a182c981d85e4d3301eba927b9c16
Karthick Raja B <120722003+krb647@users.noreply.github.com>
2023-02-13 20:01:05 +0530

    Update banker roulette.py

--- banker roulette.py
+++ banker roulette.py
@@ -1,11 +1,6 @@
-# Import the random module here
 import random
-# Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 last_name=len(names)
 person=random.randint(0,last_name-1)
 person=names[person]
